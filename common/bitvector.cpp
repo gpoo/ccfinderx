35972f3e965025e2600b806ae0ef6e3c56dcc28f
Germán Poo-Caamaño <gpoo@gnome.org>
2012-11-14 00:27:02 -0800

    Fixed compilation error
    Signed-off-by: Germán Poo-Caamaño <gpoo@gnome.org>

--- common/bitvector.cpp
+++ common/bitvector.cpp
@@ -1,35 +1,35 @@
 #include <cassert>
 #include "bitvector.h"
 
-void bitvector::resize(size_t numBits, bool defaultValue)
+void bitvector::resize(std::size_t numBits, bool defaultValue)
 {
-	size_t numBytes = (numBits + 8 - 1) / 8;
+	std::size_t numBytes = (numBits + 8 - 1) / 8;
 	body.resize(numBytes);
-	size_t old_num_bits = num_bits;
+	std::size_t old_num_bits = num_bits;
 	num_bits = numBits;
 	if (numBits > old_num_bits) {
-		size_t n = (old_num_bits + 8 - 1) / 8 * 8;
+		std::size_t n = (old_num_bits + 8 - 1) / 8 * 8;
 		assert(old_num_bits <= n && n % 8 == 0);
 		set(old_num_bits, n, defaultValue);
-		for (size_t i = n / 8 + 1; i < numBytes; ++i) {
+		for (std::size_t i = n / 8 + 1; i < numBytes; ++i) {
 			body[i] = defaultValue ? 0xff:0x00;
 		}
 	}
 }
 
-void bitvector::reserve(size_t numBits)
+void bitvector::reserve(std::size_t numBits)
 {
-	size_t numBytes = (numBits + 8 - 1) / 8;
+	std::size_t numBytes = (numBits + 8 - 1) / 8;
 	body.reserve(numBytes);
 }
 
-void bitvector::set(size_t index, bool value)
+void bitvector::set(std::size_t index, bool value)
 {
 	assert(0 <= index);
 	assert(index < num_bits);
-	size_t byte_address = index / 8;
+	std::size_t byte_address = index / 8;
 	assert(byte_address < body.size());
-	size_t bit_address = index % 8;
+	std::size_t bit_address = index % 8;
 	unsigned char mask = 1 << bit_address;
 	if (value) {
 		// set the bit
@@ -41,7 +41,7 @@ void bitvector::set(size_t index, bool value)
 	}
 }
 
-void bitvector::set(size_t begin, size_t end, bool value)
+void bitvector::set(std::size_t begin, std::size_t end, bool value)
 {
 	if (begin < 0) {
 		begin = 0;
@@ -53,11 +53,11 @@ void bitvector::set(size_t begin, size_t end, bool value)
 		return;
 	}
 
-	size_t index = begin;
+	std::size_t index = begin;
 	while (index < end) {
-		size_t byte_address = index / 8;
+		std::size_t byte_address = index / 8;
 		assert(byte_address < body.size());
-		size_t bit_address = index % 8;
+		std::size_t bit_address = index % 8;
 		if (bit_address == 0 && index + 8 <= end) {
 			body[byte_address] = value ? 0xff : 0x00;
 			index += 8;
@@ -77,12 +77,12 @@ void bitvector::set(size_t begin, size_t end, bool value)
 	}
 }
 
-bool bitvector::test(size_t index) const
+bool bitvector::test(std::size_t index) const
 {
 	assert(index < num_bits);
-	size_t byte_address = index / 8;
+	std::size_t byte_address = index / 8;
 	assert(byte_address < body.size());
-	size_t bit_address = index % 8;
+	std::size_t bit_address = index % 8;
 	unsigned char mask = 1 << bit_address;
 	if (body[byte_address] & mask) {
 		return true;
@@ -104,13 +104,13 @@ bitvector& bitvector::operator=(const bitvector& rhs)
 	return *this;
 }
 
-size_t bitvector::countValue(size_t begin, size_t end, bool value) const
+std::size_t bitvector::countValue(std::size_t begin, std::size_t end, bool value) const
 {
 	static const char bit2count[256] = {
 #include "bitcounttable.h"
 	};
 
-	size_t count = 0;
+	std::size_t count = 0;
 	if (begin < 0) {
 		begin = 0;
 	}
@@ -122,11 +122,11 @@ size_t bitvector::countValue(size_t begin, size_t end, bool value) const
 		return 0;
 	}
 
-	size_t index = begin; 
+	std::size_t index = begin; 
 	while (index < end) {
-		size_t byte_address = index / 8;
+		std::size_t byte_address = index / 8;
 		assert(byte_address < body.size());
-		size_t bit_address = index % 8;
+		std::size_t bit_address = index % 8;
 		if (bit_address == 0 && index + 8 <= end) {
 			unsigned char pat = body[byte_address];
 			count += bit2count[pat];
@@ -155,7 +155,7 @@ void bitvector::swap(bitvector &rhs)
 		return;
 	}
 
-	size_t tmp = this->num_bits; this->num_bits = rhs.num_bits; rhs.num_bits = tmp;
+	std::size_t tmp = this->num_bits; this->num_bits = rhs.num_bits; rhs.num_bits = tmp;
 	this->body.swap(rhs.body);
 }
 
--- common/bitvector.h
+++ common/bitvector.h
@@ -5,24 +5,24 @@
 
 class bitvector {
 private:
-	size_t num_bits;
+	std::size_t num_bits;
 	std:: vector<unsigned char> body;
 public:
 	inline ~bitvector() { }
 	inline bitvector() : num_bits(0), body() { }
 	inline bitvector(const bitvector& rhs) : num_bits(rhs.num_bits), body(rhs.body) { }
-	void resize(size_t numBits, bool defaultValue = false);
-	inline size_t size() const { return num_bits; }
-	void reserve(size_t numBits);
-	inline size_t capacity() const { return body.capacity() * 8; }
-	void set(size_t index, bool value);
-	void set(size_t begin, size_t end, bool value);
-	bool test(size_t index) const;
+	void resize(std::size_t numBits, bool defaultValue = false);
+	inline std::size_t size() const { return num_bits; }
+	void reserve(std::size_t numBits);
+	inline std::size_t capacity() const { return body.capacity() * 8; }
+	void set(std::size_t index, bool value);
+	void set(std::size_t begin, std::size_t end, bool value);
+	bool test(std::size_t index) const;
 	bitvector& operator=(const bitvector& rhs);
 	inline void clear() { resize(0); }
-	size_t countValue(size_t begin, size_t end, bool value) const;
-	inline size_t countTrue(size_t begin, size_t end) const { return countValue(begin, end, true); }
-	inline size_t countFalse(size_t begin, size_t end) const { return countValue(begin, end, false); }
+	std::size_t countValue(std::size_t begin, std::size_t end, bool value) const;
+	inline std::size_t countTrue(std::size_t begin, std::size_t end) const { return countValue(begin, end, true); }
+	inline std::size_t countFalse(std::size_t begin, std::size_t end) const { return countValue(begin, end, false); }
 	void swap(bitvector &rhs);
 };
 
